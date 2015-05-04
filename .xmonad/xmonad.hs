84f9433a3bf3e3e288e02d983d437b5387e65ba3
Tuukka Koistinen <tvkoisti@gmail.com>
2015-05-04 15:57:51 +0300

    xmonad: break long lines

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -32,14 +32,30 @@ myModMask = mod1Mask  -- rebind Mod to Super key
 myTerminal = "urxvt"
 myTerminalBig = "xterm -fs 25"
 myBorderWidth = 2
-myWorkspaces = ["1:web", "2:term", "3:mail", "4:terms", "5:code", "6:dev", "7:misc", "8:music", "9:vbox", "0:empty", "+:irc"]
+myWorkspaces = [ "1:web"
+               , "2:term"
+               , "3:mail"
+               , "4:terms"
+               , "5:code"
+               , "6:dev"
+               , "7:misc"
+               , "8:music"
+               , "9:vbox"
+               , "0:empty"
+               , "+:irc" ]
 
-myLayoutHook = onWorkspace "1:web" webLayout $ onWorkspace "2:term" fullLayout $ onWorkspace "3:mail" webLayout $ onWorkspace "4:terms" webLayout $ onWorkspace "8:music" verticalLayout $ onWorkspace "+:irc" noTitleLayout $ layouts
-    where layouts = smartBorders $ avoidStruts $ (layoutHook defaultConfig ||| Grid ||| ThreeCol 1 (3/100) (1/2))
-          webLayout = smartBorders $ avoidStruts (Tall 1 (3/100) (60/100) ||| Full)
-          fullLayout = smartBorders $ noBorders Full
-          noTitleLayout = smartBorders $ avoidStruts (Tall 1 (3/100) (1/2) ||| Full)
-          verticalLayout = smartBorders $ avoidStruts (Grid ||| Full)
+myLayoutHook = onWorkspace "1:web" webLayout
+             $ onWorkspace "2:term" fullLayout
+             $ onWorkspace "3:mail" webLayout
+             $ onWorkspace "4:terms" webLayout
+             $ onWorkspace "8:music" verticalLayout
+             $ onWorkspace "+:irc" noTitleLayout
+             $ layouts
+        where layouts = smartBorders $ avoidStruts $ (layoutHook defaultConfig ||| Grid ||| ThreeCol 1 (3/100) (1/2))
+              webLayout = smartBorders $ avoidStruts (Tall 1 (3/100) (60/100) ||| Full)
+              fullLayout = smartBorders $ noBorders Full
+              noTitleLayout = smartBorders $ avoidStruts (Tall 1 (3/100) (1/2) ||| Full)
+              verticalLayout = smartBorders $ avoidStruts (Grid ||| Full)
 
 myManageHook = (composeAll
     [isFullscreen                   --> doFullFloat
