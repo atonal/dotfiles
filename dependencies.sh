b6094862b74e3a5e1c6f3ce38dd3d3ec3aff5dda
Tuukka Koistinen <tvkoisti@gmail.com>
2016-12-28 12:26:34 +0200

    dependencies: copy xmonad.desktop to /usr/share/xsessions

--- dependencies.sh
+++ dependencies.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+DIR=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")
+
 #echo "Acquire::ForceIPv4 \"true\";" | sudo tee /etc/apt/apt.conf.d/99force-ipv4
 #gsettings set org.gnome.desktop.background show-desktop-icons false
 #gsettings set org.gnome.desktop.lockdown disable-lock-screen true
@@ -34,3 +37,5 @@ sudo apt-get install "${packages[@]}"
 
 sudo cabal update
 sudo cabal install --global xmobar --flags="all_extensions"
+
+cp "${DIR}"/xmonad.desktop /usr/share/xsessions/
